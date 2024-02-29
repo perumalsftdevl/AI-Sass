63a765093602b05ae3203f729a4dff02187798a8
perumal palani <perumal.p@dataspire.in>
2024-02-29 23:00:43 +0530

    mobile sidebar count added

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import SideBar from "./SideBar";
 import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
 
-const MobileSidebar = () => {
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "@/components/ui/SideBar";
+
+export const MobileSidebar = ({
+  apiLimitCount = 0,
+  isPro = false,
+}: {
+  apiLimitCount: number;
+  isPro: boolean;
+}) => {
   const [isMounted, setIsMounted] = useState(false);
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
@@ -22,18 +23,17 @@ const MobileSidebar = () => {
   if (!isMounted) {
     return null;
   }
+
   return (
     <Sheet>
-      <SheetTrigger asChild>
+      <SheetTrigger>
         <Button variant="ghost" size="icon" className="md:hidden">
           <Menu />
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-        <SideBar />
+        <SideBar isPro={isPro} apiLimitCount={apiLimitCount} />
       </SheetContent>
     </Sheet>
   );
 };
-
-export default MobileSidebar;
