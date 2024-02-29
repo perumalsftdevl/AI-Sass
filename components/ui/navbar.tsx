32725d0cabb8d48c19793f7c63a702c1fe5cbbef
perumal palani <perumal.p@dataspire.in>
2024-02-29 23:17:23 +0530

    last deploy

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,11 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
 import MobileSidebar from "./mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
-const NavBar = () => {
+const NavBar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
